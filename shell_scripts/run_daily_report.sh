5a1249db2691e31fa568075dd823b72edb25b5d1
Adi Zohar <adizohar@gmail.com>
2020-07-08 13:20:11 -0400

    Add files via upload

--- shell_scripts/run_daily_report.sh
+++ shell_scripts/run_daily_report.sh
@@ -45,8 +45,8 @@ col line for a1000
 
 prompt   <style>
 prompt        td  {font-size:9pt;font-family:arial; text-align:left;}
-prompt        th  {font-family: arial, helvetica, sans-serif; font-size: 9pt; font-weight: bold; color: #101010; background-color: #e0e0e0}
-prompt        .th1    {font-family: Arial, Helvetica, sans-serif; font-size: 9pt; font-weight: bold; color: #101010; background-color: #d0d0d0}
+prompt        th  {font-family: arial, helvetica, sans-serif; font-size: 8pt; font-weight: bold; color: #101010; background-color: #e0e0e0}
+prompt        .th1    {font-family: Arial, Helvetica, sans-serif; font-size: 8pt; font-weight: bold; color: #101010; background-color: #d0d0d0}
 prompt        .dc1   { font-family: Arial, Helvetica, sans-serif; font-size: 8pt; color: #000000; background-color: white; text-align:left;}
 prompt        .dcr   { font-family: Arial, Helvetica, sans-serif; font-size: 8pt; color: #000000; background-color: white; text-align:right;}
 prompt        .dcc   { font-family: Arial, Helvetica, sans-serif; font-size: 8pt; color: #000000; background-color: white; text-align:center;}
@@ -55,25 +55,25 @@ prompt        .tabheader {font-size:12pt; font-family:arial; font-weight:bold; c
 prompt    </style>
 
 prompt <table border=1 cellpadding=3 cellspacing=0 width=1024 >
-prompt     <tr><td colspan=15 class=tabheader>Cost Usage Report - $DATE_PRINT </td></tr>
+prompt     <tr><td colspan=15 class=tabheader>Cost Usage Daily Report - $DATE_PRINT </td></tr>
 
 select
     '<tr>'||
         '<th width=150 nowrap class=th1>Tenant Name</th>'||
-        '<th width=70  nowrap class=th1>'||to_char(trunc(sysdate-10),'DD-MON-YYYY')||'</th>'||
-        '<th width=70  nowrap class=th1>'||to_char(trunc(sysdate-9),'DD-MON-YYYY')||'</th>'||
-        '<th width=70  nowrap class=th1>'||to_char(trunc(sysdate-8),'DD-MON-YYYY')||'</th>'||
-        '<th width=70  nowrap class=th1>'||to_char(trunc(sysdate-7),'DD-MON-YYYY')||'</th>'||
-        '<th width=70  nowrap class=th1>'||to_char(trunc(sysdate-6),'DD-MON-YYYY')||'</th>'||
-        '<th width=70  nowrap class=th1>'||to_char(trunc(sysdate-5),'DD-MON-YYYY')||'</th>'||
-        '<th width=70  nowrap class=th1>'||to_char(trunc(sysdate-4),'DD-MON-YYYY')||'</th>'||
-        '<th width=70  nowrap class=th1>'||to_char(trunc(sysdate-3),'DD-MON-YYYY')||'</th>'||
-        '<th width=70  nowrap class=th1>'||to_char(trunc(sysdate-2),'DD-MON-YYYY')||'</th>'||
-        '<th width=70  nowrap class=th1>'||to_char(trunc(sysdate-1),'DD-MON-YYYY')||'</th>'||
-        '<th width=70  nowrap class=th1>Month 31 Prediction</th>'||
-        '<th width=70  nowrap class=th1>Year Prediction</th>'||
-        '<th width=70  nowrap class=th1>Last Load</th>'||
-        '<th width=70  nowrap class=th1>Agent Used</th>'||
+        '<th width=80  nowrap class=th1>'||to_char(trunc(sysdate-10),'DD-MON-YYYY')||'</th>'||
+        '<th width=80  nowrap class=th1>'||to_char(trunc(sysdate-9),'DD-MON-YYYY')||'</th>'||
+        '<th width=80  nowrap class=th1>'||to_char(trunc(sysdate-8),'DD-MON-YYYY')||'</th>'||
+        '<th width=80  nowrap class=th1>'||to_char(trunc(sysdate-7),'DD-MON-YYYY')||'</th>'||
+        '<th width=80  nowrap class=th1>'||to_char(trunc(sysdate-6),'DD-MON-YYYY')||'</th>'||
+        '<th width=80  nowrap class=th1>'||to_char(trunc(sysdate-5),'DD-MON-YYYY')||'</th>'||
+        '<th width=80  nowrap class=th1>'||to_char(trunc(sysdate-4),'DD-MON-YYYY')||'</th>'||
+        '<th width=80  nowrap class=th1>'||to_char(trunc(sysdate-3),'DD-MON-YYYY')||'</th>'||
+        '<th width=80  nowrap class=th1>'||to_char(trunc(sysdate-2),'DD-MON-YYYY')||'</th>'||
+        '<th width=80  nowrap class=th1>'||to_char(trunc(sysdate-1),'DD-MON-YYYY')||'</th>'||
+        '<th width=80  nowrap class=th1>Month 31 Prediction</th>'||
+        '<th width=80  nowrap class=th1>Year Prediction</th>'||
+        '<th width=80  nowrap class=th1>Last Load</th>'||
+        '<th width=80  nowrap class=th1>Agent Used</th>'||
     '</tr>'
     as line
 from dual
@@ -135,6 +135,87 @@ from
     )
 );
 prompt </table>
+prompt <br><br>
+
+prompt <table border=1 cellpadding=3 cellspacing=0 width=1024 >
+prompt     <tr><td colspan=15 class=tabheader>Cost Usage Monthly Report - $DATE_PRINT </td></tr>
+
+select
+    '<tr>'||
+        '<th width=150 nowrap class=th1>Tenant Name</th>'||
+        '<th width=70  nowrap class=th1>'||to_char(add_months(trunc(sysdate,'MM'),-11),'MON-YYYY')||'</th>'||
+        '<th width=70  nowrap class=th1>'||to_char(add_months(trunc(sysdate,'MM'),-10),'MON-YYYY')||'</th>'||
+        '<th width=70  nowrap class=th1>'||to_char(add_months(trunc(sysdate,'MM'), -9),'MON-YYYY')||'</th>'||
+        '<th width=70  nowrap class=th1>'||to_char(add_months(trunc(sysdate,'MM'), -8),'MON-YYYY')||'</th>'||
+        '<th width=70  nowrap class=th1>'||to_char(add_months(trunc(sysdate,'MM'), -7),'MON-YYYY')||'</th>'||
+        '<th width=70  nowrap class=th1>'||to_char(add_months(trunc(sysdate,'MM'), -6),'MON-YYYY')||'</th>'||
+        '<th width=70  nowrap class=th1>'||to_char(add_months(trunc(sysdate,'MM'), -5),'MON-YYYY')||'</th>'||
+        '<th width=70  nowrap class=th1>'||to_char(add_months(trunc(sysdate,'MM'), -4),'MON-YYYY')||'</th>'||
+        '<th width=70  nowrap class=th1>'||to_char(add_months(trunc(sysdate,'MM'), -3),'MON-YYYY')||'</th>'||
+        '<th width=70  nowrap class=th1>'||to_char(add_months(trunc(sysdate,'MM'), -2),'MON-YYYY')||'</th>'||
+        '<th width=70  nowrap class=th1>'||to_char(add_months(trunc(sysdate,'MM'), -1),'MON-YYYY')||'</th>'||
+        '<th width=70  nowrap class=th1>'||to_char(           trunc(sysdate,'MM')    ,'MON-YYYY')||'</th>'||
+    '</tr>'
+    as line
+from dual
+union all
+select
+    '<tr>'||
+        '<td nowrap class=dc1> '||tenant_name||'</td> '||
+        '<td nowrap class='||m12_class||'> '||to_char(m12,'999,999,999')||'</td>'||
+        '<td nowrap class='||m11_class||'> '||to_char(m11,'999,999,999')||'</td>'||
+        '<td nowrap class='||m10_class||'> '||to_char(m10,'999,999,999')||'</td>'||
+        '<td nowrap class='|| m9_class||'> '||to_char( m9,'999,999,999')||'</td>'||
+        '<td nowrap class='|| m8_class||'> '||to_char( m8,'999,999,999')||'</td>'||
+        '<td nowrap class='|| m7_class||'> '||to_char( m7,'999,999,999')||'</td>'||
+        '<td nowrap class='|| m6_class||'> '||to_char( m6,'999,999,999')||'</td>'||
+        '<td nowrap class='|| m5_class||'> '||to_char( m5,'999,999,999')||'</td>'||
+        '<td nowrap class='|| m4_class||'> '||to_char( m4,'999,999,999')||'</td>'||
+        '<td nowrap class='|| m3_class||'> '||to_char( m3,'999,999,999')||'</td>'||
+        '<td nowrap class='|| m2_class||'> '||to_char( m2,'999,999,999')||'</td>'||
+        '<td nowrap class='|| m1_class||'> '||to_char( m1,'999,999,999')||'</td>'||
+    '</tr>' as line
+from
+(
+    select
+        tenant_name,m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,
+        case when trunc(greatest(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12)) = trunc(m1)  then 'dcbold' else 'dcr' END as m1_class,
+        case when trunc(greatest(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12)) = trunc(m2)  then 'dcbold' else 'dcr' END as m2_class,
+        case when trunc(greatest(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12)) = trunc(m3)  then 'dcbold' else 'dcr' END as m3_class,
+        case when trunc(greatest(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12)) = trunc(m4)  then 'dcbold' else 'dcr' END as m4_class,
+        case when trunc(greatest(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12)) = trunc(m5)  then 'dcbold' else 'dcr' END as m5_class,
+        case when trunc(greatest(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12)) = trunc(m6)  then 'dcbold' else 'dcr' END as m6_class,
+        case when trunc(greatest(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12)) = trunc(m7)  then 'dcbold' else 'dcr' END as m7_class,
+        case when trunc(greatest(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12)) = trunc(m8)  then 'dcbold' else 'dcr' END as m8_class,
+        case when trunc(greatest(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12)) = trunc(m9)  then 'dcbold' else 'dcr' END as m9_class,
+        case when trunc(greatest(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12)) = trunc(m10) then 'dcbold' else 'dcr' END as m10_class,
+        case when trunc(greatest(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12)) = trunc(m11) then 'dcbold' else 'dcr' END as m11_class,
+        case when trunc(greatest(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12)) = trunc(m12) then 'dcbold' else 'dcr' END as m12_class
+    from
+    (
+        select 
+            tenant_name,
+            sum(case when trunc(USAGE_INTERVAL_START,'MM') = add_months(trunc(sysdate,'MM'),-11) then COST_MY_COST else 0 end) M12,
+            sum(case when trunc(USAGE_INTERVAL_START,'MM') = add_months(trunc(sysdate,'MM'),-10) then COST_MY_COST else 0 end) M11,
+            sum(case when trunc(USAGE_INTERVAL_START,'MM') = add_months(trunc(sysdate,'MM'), -9) then COST_MY_COST else 0 end) M10,
+            sum(case when trunc(USAGE_INTERVAL_START,'MM') = add_months(trunc(sysdate,'MM'), -8) then COST_MY_COST else 0 end) M9,
+            sum(case when trunc(USAGE_INTERVAL_START,'MM') = add_months(trunc(sysdate,'MM'), -7) then COST_MY_COST else 0 end) M8,
+            sum(case when trunc(USAGE_INTERVAL_START,'MM') = add_months(trunc(sysdate,'MM'), -6) then COST_MY_COST else 0 end) M7,
+            sum(case when trunc(USAGE_INTERVAL_START,'MM') = add_months(trunc(sysdate,'MM'), -5) then COST_MY_COST else 0 end) M6,
+            sum(case when trunc(USAGE_INTERVAL_START,'MM') = add_months(trunc(sysdate,'MM'), -4) then COST_MY_COST else 0 end) M5,
+            sum(case when trunc(USAGE_INTERVAL_START,'MM') = add_months(trunc(sysdate,'MM'), -3) then COST_MY_COST else 0 end) M4,
+            sum(case when trunc(USAGE_INTERVAL_START,'MM') = add_months(trunc(sysdate,'MM'), -2) then COST_MY_COST else 0 end) M3,
+            sum(case when trunc(USAGE_INTERVAL_START,'MM') = add_months(trunc(sysdate,'MM'), -1) then COST_MY_COST else 0 end) M2,
+            sum(case when trunc(USAGE_INTERVAL_START,'MM') =            trunc(sysdate,'MM')      then COST_MY_COST else 0 end) M1
+        from oci_cost_stats
+        where 
+            tenant_name like '%'
+        group by tenant_name
+        order by 1
+    )
+);
+prompt </table>
+
 " | sqlplus -s ${DATABASE_USER}/${DATABASE_PASS}@${DATABASE_NAME} > $OUTPUT_FILE
 
 # Check for errors
